b51e594e1947d61296c628044dcfe8794db9f739
Naasom Rodrigues <1369597@sga.pucminas.br>
2024-12-04 16:40:07 -0300

    readicionando

--- src/serverLC.js
+++ src/serverLC.js
@@ -73,7 +73,7 @@ database.sequelize.authenticate().then(function () {
 app.use(express.json());
 
 
-const port = 30079;
+const port = process.env.PORT || 30079;
 const SECRET_KEY = 'sua_chave_secreta';
 
 app.use(cors());
