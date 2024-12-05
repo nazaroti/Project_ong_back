039adf9ef61a6292fce1f51c79b999823f2243ce
Naasom Rodrigues <1369597@sga.pucminas.br>
2024-12-05 16:07:58 -0300

    cors apdate

--- src/serverLC.js
+++ src/serverLC.js
@@ -79,9 +79,16 @@ const SECRET_KEY = 'sua_chave_secreta';
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Permite qualquer origem
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    // Permitir preflight requests (opcional, mas útil para algumas configurações de CORS)
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    
     next();
 });
+
 
 // Função para gerar o token JWT
 function gerarToken(id) {
