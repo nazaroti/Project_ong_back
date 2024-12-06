996bdee77771be68eeb46a66ef1b9702bdb744dd
Naasom Rodrigues <1369597@sga.pucminas.br>
2024-12-06 16:10:41 -0300

    correção

--- src/serverLC.js
+++ src/serverLC.js
@@ -86,7 +86,7 @@ app.use((req, res, next) => {
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin);
     }
-S
+
     // Métodos e cabeçalhos permitidos
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
