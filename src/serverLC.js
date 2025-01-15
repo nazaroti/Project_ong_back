8e2a6c82287948927305b4df31d79f4fbf76d1d6
Naasom Rodrigues <1369597@sga.pucminas.br>
2025-01-15 13:10:32 -0300

    atualizacoes

--- src/serverLC.js
+++ src/serverLC.js
@@ -882,7 +882,7 @@ cron.schedule('0 0 * * *', async () => {
     }
 });
 
-app.get('/', async (req, res) => {
+/*app.get('/', async (req, res) => {
     
     const nome = 'Admin'
     const email = 'rosa.6579100@pucminas.br'
@@ -906,6 +906,7 @@ app.get('/', async (req, res) => {
         res.status(500).send({ message: 'Erro ao cadastrar usuário.' });
     }
 });
+*/
 
 // #endregion
 
